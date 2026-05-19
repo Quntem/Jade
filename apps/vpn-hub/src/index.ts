f123db80a2adc5296471b82024969891488b51e0
oscar <oscar@quntem.co.uk>
2026-05-19 21:20:55 +0100

    more changes

--- apps/vpn-hub/src/index.ts
+++ apps/vpn-hub/src/index.ts
@@ -332,7 +332,6 @@ async function applyHubConfig({
 
   if (isWireGuardToolsBackend(config.applyBackend)) {
     await runOptionalCommand("ip", ["link", "delete", "dev", config.interfaceName]);
-    await runCommand("ip", ["link", "add", "dev", config.interfaceName, "type", "wireguard"]);
 
     await wireguardTools.setConfig(config.interfaceName, {
       privateKey,
